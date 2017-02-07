b0e6fab9482229b29f84c4f7799e17feb5541e34
Kyle Gengler <kyle.gengler@gmail.com>
2017-02-07 07:22:27 -0600

    more information from blog

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Basic Web Application
 
+Code sample for post at
+[https://kylegengler.com/java/spring/2017/02/07/basic-spring-web-application-with-no-xml.html](https://kylegengler.com/java/spring/2017/02/07/basic-spring-web-application-with-no-xml.html).
+
 An example of a basic Spring 4 application with a Thymeleaf view resolver using no XML.
+
--- src/main/java/com/kylegengler/basic/ServletConfiguration.java
+++ src/main/java/com/kylegengler/basic/ServletConfiguration.java
@@ -15,6 +15,15 @@ import org.thymeleaf.templatemode.TemplateMode;
 /**
  * Configure the Spring dispatcher servlet
  * 
+ * The configuration annotation designates this as a Spring configuration.
+ * EnableWebMvc - tells spring to import and set up any configuration needed for
+ * the controllers and any other mvc components. ComponentScan - Tells spring
+ * the base package(s) to scan on the classpath for any spring @Components, or
+ * any of its derivitives such as @Service, @Controller, or @Repository
+ * 
+ * This file will specify any @Beans that need to be managed by the Spring IoC                                                                                                  
+ * container for Dependency Injection
+ * 
  * @author kyle
  */
 @Configuration
@@ -27,7 +36,8 @@ public class ServletConfiguration extends WebMvcConfigurerAdapter {
 
 	/**
 	 * Set up the template resolver. This will look for templates in the
-	 * /WEB-INF/templates directory, ending with html.
+	 * /WEB-INF/templates directory, ending with html. This is used to find the
+	 * template files needed to generate the view
 	 */
 	@Bean
 	public SpringResourceTemplateResolver templateResolver() {
@@ -40,7 +50,9 @@ public class ServletConfiguration extends WebMvcConfigurerAdapter {
 		return templateResolver;
 	}
 
-	
+	/**
+	 * The engine used to render the view using the template file and model.
+	 */
 	@Bean
 	public SpringTemplateEngine templateEngine() {
 		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
@@ -49,6 +61,11 @@ public class ServletConfiguration extends WebMvcConfigurerAdapter {
 		return templateEngine;
 	}
 
+	/**
+	 * The view resolver receives the name of the view, and is responsible for
+	 * creating the corresponding View object to go with that view. It ties
+	 * together the Controller logic and template engine.
+	 */
 	@Bean
 	public ThymeleafViewResolver viewResolver() {
 		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
